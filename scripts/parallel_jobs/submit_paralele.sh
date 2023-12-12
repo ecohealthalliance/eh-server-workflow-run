1758e195396fb9e4e80b0ddd146a8b15f9d8342b
Andrew Espira <espirado@controller.local.dev>
2023-12-12 09:14:10 -0500

    update scripts

--- scripts/parallel_jobs/submit_paralele.sh
+++ scripts/parallel_jobs/submit_paralele.sh
@@ -6,5 +6,14 @@
 #SBATCH --time=01:00:00             # Time limit hrs:min:sec (optional)
 #SBATCH --mem=2000                  # Memory needed (optional)
 
-                      # Load R module (adjust if necessary)
-Rscript parallel_script.R           # Run the R script
+                
+
+# Run the R script
+/usr/local/bin/Rscript   parallel_script.R
+
+
+
+# Use sacct to get resource usage and append it to a fileormat=JobID,JobName,Partition,MaxRSS,Elapsed,State,CPUTime,TotalCPU >> ~/eh-server-workflow-run/test/resource_usage.txt
+
+
+sacct -j $SLURM_JOB_ID --format=JobID,JobName,Partition,MaxRSS,Elapsed,State,CPUTime,TotalCPU >> resource_usage.txt
